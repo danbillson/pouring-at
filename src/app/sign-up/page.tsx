e25d92a2df85b1e8e44dd19fa0e1ff2e5d39881f
Dan Billson <dbillson@outlook.com>
2025-04-05 23:04:20 +0100

    refactor: update sign up link

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,16 +1,20 @@
 import { SignUpForm } from "@/components/sign-up-form";
 import { Beer } from "lucide-react";
+import Link from "next/link";
 
 export default function SignUpPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <Link
+          href="/"
+          className="flex items-center gap-2 self-center font-medium"
+        >
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
             <Beer className="size-4" />
           </div>
           pouring.at
-        </a>
+        </Link>
         <SignUpForm />
       </div>
     </div>
