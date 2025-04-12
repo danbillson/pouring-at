e6a915cabae63d723eedb1efbd12332cfd1faa00
Dan Billson <daniel.billson@paddle.com>
2025-04-13 00:26:51 +0100

    feat: add simple bar fetch

--- src/app/bars/[slug]/page.tsx
+++ src/app/bars/[slug]/page.tsx
@@ -1,13 +1,29 @@
-interface PageProps {
-  params: Promise<{ slug: string }>;
-}
+import { db } from "@/db";
+import { bar } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
 
-export default async function BarPage({ params }: PageProps) {
+export default async function BarPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
 
+  const [barData] = await db.select().from(bar).where(eq(bar.slug, slug));
+
+  if (!barData) {
+    notFound();
+  }
+
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-2xl font-bold">Bar: {slug}</h1>
+    <div className="bg-muted flex min-h-svh flex-col items-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <h1 className="text-4xl font-bold">{barData.name}</h1>
+        <p className="text-muted-foreground text-sm">
+          {barData.formattedAddress}
+        </p>
+      </div>
     </div>
   );
 }
