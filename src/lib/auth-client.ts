e3e43e7fd16504daead6446b5df649a6ab6e45c8
Dan Billson <daniel.billson@paddle.com>
2025-04-20 22:00:41 +0100

    feat: add admin and org auth plugins

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,18 @@
+import { ac, admin, member, user as userPermissions } from "@/lib/permissions";
+import { adminClient, organizationClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const { signIn, signUp, signOut, useSession, updateUser } =
-  createAuthClient();
+  createAuthClient({
+    plugins: [
+      organizationClient(),
+      adminClient({
+        ac,
+        roles: {
+          admin,
+          member,
+          user: userPermissions,
+        },
+      }),
+    ],
+  });
