943111a5515fb8fcb43efc18118d78a516bde17f
Dan Billson <daniel.billson@paddle.com>
2025-04-20 12:04:09 +0100

    feat: add profile image upload functionality and related components; update environment variables for Supabase integration

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,8 +1,13 @@
+import { ProfileImageUpload } from "@/components/dashboard/profile-image-upload";
 import { SignOut } from "@/components/dashboard/sign-out";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+
+const storageUrl = process.env.NEXT_PUBLIC_STORAGE_URL!;
 
 export default async function ProfilePage() {
   const session = await auth.api.getSession({
@@ -22,6 +27,23 @@ export default async function ProfilePage() {
           </CardHeader>
           <CardContent>
             <div className="grid gap-4">
+              <div>
+                <p className="text-muted-foreground mb-4 text-sm">
+                  Profile image
+                </p>
+                <div className="relative w-fit">
+                  <Avatar className="size-24">
+                    <AvatarImage src={`${storageUrl}/${session.user.image}`} />
+                    <AvatarFallback className="bg-foreground text-background text-2xl uppercase">
+                      {session.user.name.charAt(0)}
+                    </AvatarFallback>
+                  </Avatar>
+                  <ProfileImageUpload userId={session.user.id} />
+                </div>
+              </div>
+
+              <Separator />
+
               <div>
                 <p className="text-muted-foreground text-sm">Email</p>
                 <p>{session.user.email}</p>
