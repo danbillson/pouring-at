faaec22e8f6b9df25e88dc08277e12ef10a694e2
Dan Billson <daniel.billson@paddle.com>
2025-04-21 19:01:24 +0100

    feat: add brewery update route

--- src/app/api/breweries/[id]/route.ts
+++ src/app/api/breweries/[id]/route.ts
@@ -1,6 +1,9 @@
 import { db } from "@/db";
 import { brewery } from "@/db/schema";
+import { hasAccessToBrewery } from "@/lib/access";
+import { auth } from "@/lib/auth";
 import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -17,4 +20,32 @@ export async function GET(
   }
 
   return NextResponse.json({ brewery: result });
+}
+
+export async function PATCH(request: Request) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  const userBrewery = await hasAccessToBrewery();
+
+  if (!userBrewery) {
+    return new NextResponse("Not found", { status: 404 });
+  }
+
+  const data = await request.json();
+
+  await db
+    .update(brewery)
+    .set({
+      ...data,
+      updatedAt: new Date(),
+    })
+    .where(eq(brewery.id, userBrewery.id));
+
+  return NextResponse.json({ success: true });
 }
