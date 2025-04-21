6f92969f20a02cfda142b29e866cc8abd1365983
Dan Billson <daniel.billson@paddle.com>
2025-04-21 16:57:33 +0100

    feat: enhance BeerPage layout by displaying beer style alongside name and adjusting ABV display for better visibility

--- src/app/(public)/beers/[id]/page.tsx
+++ src/app/(public)/beers/[id]/page.tsx
@@ -17,17 +17,19 @@ export default async function BeerPage({
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <h1 className="text-4xl font-bold">{beerData.name}</h1>
+        <h1 className="text-4xl font-bold">
+          {beerData.name}{" "}
+          <span className="text-muted-foreground">{beerData.style}</span>
+        </h1>
         <Link
           href={`/breweries/${beerData.brewery?.id}`}
           className="text-muted-foreground text-sm hover:underline"
         >
           {beerData.brewery?.name}
         </Link>
-        <p className="text-muted-foreground text-sm">
-          {beerData.style}
+        <span className="text-muted-foreground text-sm">
           {beerData.abv && ` • ${beerData.abv}%`}
-        </p>
+        </span>
       </div>
     </div>
   );
