baffc1ea81ec5f633a21afdec50a06a4564c5274
Dan Billson <daniel.billson@paddle.com>
2025-04-27 16:36:03 +0100

    style: update theme

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -3,37 +3,35 @@ import { TextLoop } from "@/components/ui/text-loop";
 
 export default function Home() {
   return (
-    <div className="bg-muted m-2 grid min-h-[calc(100vh-84px)] rounded-lg p-8 pb-20 sm:p-20">
-      <div className="mx-auto w-full max-w-5xl">
-        <h1 className="text-2xl font-bold sm:text-4xl lg:text-6xl">
-          Searching for a{" "}
-          <TextLoop interval={3}>
-            <Em>Brown Ale</Em>
-            <Em>Porter</Em>
-            <Em>Sour</Em>
-            <Em>IPA</Em>
-          </TextLoop>
-          <br />
-          in{" "}
-          <TextLoop interval={3}>
-            <Em>Newcastle?</Em>
-            <Em>London?</Em>
-            <Em>Edinburgh?</Em>
-            <Em>Manchester?</Em>
-          </TextLoop>
-        </h1>
-        <p className="mt-4 max-w-2xl text-lg">
-          We&apos;ve got you covered. Find all of your favourite beers with
-          real-time tap lists, personalized recommendations based on your taste,
-          and a map to guide you to your next perfect pint.
-        </p>
-        <div className="mt-10 flex flex-col">
-          <h3 className="mb-2 text-lg font-bold">Find bars near you</h3>
-          <LocationInput
-            placeholder="Enter a town, city or postcode..."
-            className="bg-background h-10 px-4 py-2 pl-9 md:text-lg"
-          />
-        </div>
+    <div className="mx-auto w-full max-w-5xl">
+      <h1 className="text-2xl font-bold sm:text-4xl lg:text-6xl">
+        Searching for a{" "}
+        <TextLoop interval={3}>
+          <Em>Brown Ale</Em>
+          <Em>Porter</Em>
+          <Em>Sour</Em>
+          <Em>IPA</Em>
+        </TextLoop>
+        <br />
+        in{" "}
+        <TextLoop interval={3}>
+          <Em>Newcastle?</Em>
+          <Em>London?</Em>
+          <Em>Edinburgh?</Em>
+          <Em>Manchester?</Em>
+        </TextLoop>
+      </h1>
+      <p className="mt-4 max-w-2xl text-lg">
+        We&apos;ve got you covered. Find all of your favourite beers with
+        real-time tap lists, personalized recommendations based on your taste,
+        and a map to guide you to your next perfect pint.
+      </p>
+      <div className="mt-10 flex flex-col">
+        <h3 className="mb-2 text-lg font-bold">Find bars near you</h3>
+        <LocationInput
+          placeholder="Enter a town, city or postcode..."
+          className="bg-background h-10 px-4 py-2 pl-9 md:text-lg"
+        />
       </div>
     </div>
   );
