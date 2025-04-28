a06ff062671eedef37fc5cfb59fd51980a56fd31
Dan Billson <daniel.billson@paddle.com>
2025-04-28 08:17:54 +0100

    chore: add style search back

--- src/components/forms/beer-style-select.tsx
+++ src/components/forms/beer-style-select.tsx
@@ -54,7 +54,7 @@ export function BeerStyleSelect({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[--radix-popover-trigger-width] p-0">
+      <PopoverContent className="w-[400px] p-0">
         <Command>
           <CommandInput placeholder="Search style..." />
           <CommandList>
